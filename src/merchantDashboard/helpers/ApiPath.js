2f5dfd3edb2cdc562806d3278fdb335b88a14c50
vamshithallapalli <thallapallivamshi50@gmail.com>
2025-03-20 12:12:51 +0530

    changed path to live api url

--- src/merchantDashboard/helpers/ApiPath.js
+++ src/merchantDashboard/helpers/ApiPath.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:4000";
 
-export const API_URL = "https://fdapp-backend.onrender.com/";
+export const API_URL = "https://cravxpress-backend.onrender.com/";
