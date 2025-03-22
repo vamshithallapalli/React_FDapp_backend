cb77e73b8d411c755204f9632742dbc51bce9dfc
vamshithallapalli <thallapallivamshi50@gmail.com>
2025-03-22 18:42:08 +0530

    changed welcome

--- src/merchantDashboard/components/Welcome.jsx
+++ src/merchantDashboard/components/Welcome.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Welcome = () => {
   return (
     <div>
-      <h1>Welcome to FoodDeliveryApp</h1>
+      <h1 style="text-align: center;">Welcome back! Your dashboard is waiting...</h1>
+
     </div>
   );
 };
